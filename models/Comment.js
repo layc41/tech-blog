1709932a218a64fb3844c839b6f96b871ffbb431
layc41 <64671431+layc41@users.noreply.github.com>
2020-10-24 18:21:31 -0500

    added file structure

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Comment;
